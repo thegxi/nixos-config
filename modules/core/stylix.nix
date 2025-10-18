e385b40b50c04e006570788e53a7652cf0ebceb4
thegx <village.cowherd@gmail.com>
2025-10-18 09:53:03 +0800

    fix: change inherit variable name

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -4,7 +4,7 @@
   lib,
   ...
 }: let
-  inherit (import ../../hosts/${host}/variables.nix) stylix-image stylix-enable;
+  inherit (import ../../hosts/${host}/variables.nix) stylixImage stylixEnable;
 in
 lib.mkIf stylixEnable {
   # Styling Options
